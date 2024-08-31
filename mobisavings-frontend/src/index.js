4505733bd6844959c1e0e3d5a099fe23e8127083
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-08-31 06:15:31 +0300

    Update index.js

--- mobisavings-frontend/src/index.js
+++ mobisavings-frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';  // Import global CSS styles
+import './styles.css';  // Importing global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
