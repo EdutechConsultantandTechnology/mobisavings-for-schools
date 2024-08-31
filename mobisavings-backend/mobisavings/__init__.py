f2d1124ff48feb6d6a85831cf6b54b8c3c6cd0d9
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-08-31 04:49:56 +0300

    Create __init__.py

---
+++ mobisavings-backend/mobisavings/__init__.py
@@ -0,0 +1,11 @@
+# mobisavings/__init__.py
+
+# This file marks the directory as a Python package and can also include initialization code if necessary.
+
+# You can include any package-level docstring or initializations here.
+
+"""
+Mobisavings Package
+-------------------
+This is the main package for the Mobisavings project.
+"""
