02b60eda90975b453c09930d3cde2c1e041297a7
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-08-31 06:05:57 +0300

    Create Header.js

---
+++ mobisavings-frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';  // Importing styles specific to Header
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>MobiSavings</h1>
+        </header>
+    );
+}
+
+export default Header;
