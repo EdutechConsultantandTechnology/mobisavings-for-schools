11538f8b5e84ba9e1e73b59ac641c0395cf675da
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-08-31 06:15:01 +0300

    Update App.js

--- mobisavings-frontend/src/App.js
+++ mobisavings-frontend/src/App.js
@@ -14,6 +14,7 @@ import EducationPage from './pages/EducationPage';
 import AdminDashboard from './pages/AdminDashboard';
 import LoanPage from './pages/LoanPage';
 import './styles.css';
+import './styles.css';  // Importing global styles
 
 const App = () => {
   return (
