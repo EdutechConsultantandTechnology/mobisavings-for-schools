c6e8ac4e6af7e227c1c51342d6c8d70c1fe988c6
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-08-31 04:56:07 +0300

    Create logging.py

---
+++ mobisavings-backend/mobisavings/logging.py
@@ -0,0 +1,16 @@
+# logging.py
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
